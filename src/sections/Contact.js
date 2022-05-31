eec9dd84386292e705d5c9b7b232cbe2e3f4e0d4
NoamShafir <classroomnoam@gmail.com>
2022-05-31 04:40:15 +0300

    Minor style changes

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,6 +1,7 @@
 import './Contact.css';
-import { ReactComponent as LinkedInLogo } from '../assets/icons/linkedin.svg';
 import { ReactComponent as EmailLogo } from '../assets/icons/email.svg';
+import { ReactComponent as PhoneLogo } from '../assets/icons/phone.svg';
+import { ReactComponent as LinkedInLogo } from '../assets/icons/linkedin.svg';
 
 const Contact = () => {
   return (
@@ -10,14 +11,21 @@ const Contact = () => {
         <h1>Contact</h1>
         <br/>
         <div className='option'>
-            <EmailLogo fill='white' />
+            <EmailLogo className='logo' fill='white' />
             <a href='mailto:johndoe@gmail.com' className='address' target="_blank" rel="noopener noreferrer">
                 <h3>johndoe@gmail.com</h3>
             </a>
         </div>
         <br/>
         <div className='option'>
-            <LinkedInLogo className='linkedin-logo' />
+            <PhoneLogo className='logo' fill='white' />
+            <a href='tel:1234567890' className='address' target="_blank" rel="noopener noreferrer">
+                <h3>123-4567-890</h3>
+            </a>
+        </div>
+        <br/>
+        <div className='option'>
+            <LinkedInLogo className='logo linkedin' />
             <a href='https://www.linkedin.com/in/johndoe/' className='address' target="_blank" rel="noopener noreferrer">
                 <h3>linkedin.com/in/johndoe</h3>
             </a>
